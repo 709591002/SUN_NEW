2608811ca518c603137a6802d63fd16df50a6123
实验室超级电脑 <sun_sw_2016@outlook.com>
2016-02-25 16:36:32 +0800

    增加相关时间统计

--- SUN_NEW/main.cpp
+++ SUN_NEW/main.cpp
@@ -5,11 +5,27 @@
 #include "multiNet.h"
 int main()
 {
+	//开始计时器
+	time_t start = 0, end = 0;
+	time(&start);
+
 	//设置默认字体
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_INTENSITY);
 
+	network n1(100000,DIRECT_WITH_WEIGHT);
 
+	cout << "创建完毕" << endl;
 
+	for (int i = 1; i < n1.nodeNum; i++)
+	{
+		n1.addLinkToNetwork(i, i + 1, 0.999);
+	}
+
+	n1.outputNetwork("aaa/sss.txt");
+
+	//结束计时
+	time(&end);
+	cout << "程序整体运行了：" << (end - start) << "秒" << endl;
 	return 0;
 }
 
